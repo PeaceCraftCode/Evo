094e7dc60f35c0cd4440b5c543ee2e751c0eade8
iTecX <30746447+iTecAI@users.noreply.github.com>
2019-10-10 11:34:43 -0400

    Add some base classes & utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+def cap(value,upper=1.0,lower=0.0): #keeps <value> between upper and lower bounds
+    if value > upper:
+        value = upper
+    elif value < lower:
+        value = lower
+    return values
+
+class Object: #basic object class, subclass to add functionality
+    def objinit(self):
+        pass
+    def __init__(self,pos,**kwargs):
+        self.pos = pos
+        for k in kwargs.keys():
+            setattr(self,k,kwargs[k])
+        self.objinit()
+
+class Plant(Object):
+    def objinit(self):
+        self.curFoodPerc = 1.0
+        self.alive = True
+    def eat(self,consumer,perc=1.0):
+        perc = cap(perc,upper=self.curFoodPerc)
+        self.curFoodPerc -= perc
+        consumer.food += perc * self.foodValue
+        if self.curFoodPerc <= 0.0:
+            self.alive = False
+
+
+
+
