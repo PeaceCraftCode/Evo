0a0e1a6073bf2271d004cd92cc1e58c7a9cb706e
iTecAI <matteovh@gmail.com>
2019-10-12 17:31:06 -0400

    Add comments to previous edits in utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import time
 import random
 import math
 
-GENEMINMAXMEAN = {
+GENEMINMAXMEAN = { #Dictionary of [min,max,mean,std. dev] of genes.
     'size' : [1,100,50,10],
     'attractiveness' : [.2,2,1,.2],
     'neckLength' : [.1, 5,.5,.1],
@@ -19,10 +19,10 @@ def cap(value,upper=1.0,lower=0.0): #keeps <value> between upper and lower bound
 def doPythag(pos1,pos2): #find the pythagorean distance between 2 points
     return math.sqrt((pos2[0]-pos1[0])**2+(pos2[1]-pos1[1])**2)
 
-def getAngle(pos1,pos2):
+def getAngle(pos1,pos2): #find angle between 2 points, based on a +x vector
     return math.degrees(math.atan2(pos1[1]-pos2[1], pos2[0]-pos1[0]))
 
-def sort_array(array,index):
+def sort_array(array,index): #sort 2d list based on one index
     sortedinit = sorted([i[index] for i in array])
     out = []
     for i in sortedinit:
@@ -40,7 +40,7 @@ class World: #World class, manages pathfinding requests
     def __init__(self,config,size):
         self.config = config
         self.objects = []
-        self.size = size
+        self.size = size #world size, tuple
 
 class Object: #basic object class, subclass to add functionality
     def objinit(self):
@@ -54,18 +54,19 @@ class Object: #basic object class, subclass to add functionality
             setattr(self,k,kwargs[k])
         self.objinit()
     
-    def get_visibility(self):
+    def get_visibility(self): #get visibility coeff, used in find_target()
         return 1
 
-    def tick(self):
+    def tick(self): #to run every tick
         pass
+
     def move(self,direct,spd): #move spd pointed direct
         xd = (math.cos(math.radians(direct)))/spd
         yd = (math.sin(math.radians(direct)))/spd
         self.pos[0]+=xd
         self.pos[1]+=yd
 
-class Plant(Object): #basic plant
+class Plant(Object): #basic plant, pass type and foodValue args
     def objinit(self):
         self.curFoodPerc = 1.0
         self.justGrew = False
@@ -80,14 +81,14 @@ class Plant(Object): #basic plant
         if self.curFoodPerc <= 0.0:
             self.alive = False
     
-    def get_size(self):
+    def get_size(self): #calculate size
         return self.foodValue * 5 * (self.curFoodPerc/2 + .5)
 
     def tick(self): #run every game tick
         if round(time.time()) % self.growTime == 0 and not self.justGrew and self.curFoodPerc < 1.0:
             self.curFoodPerc += self.growAmt
 
-class Animal(Object): #basic animal
+class Animal(Object): #basic animal, pass specGenes (default species genes), deviation (percentage of range)
     def objinit(self):
         global GENEMINMAXMEAN
         self.genes = {}
@@ -96,14 +97,14 @@ class Animal(Object): #basic animal
             self.genes[gene] = cap((self.specGenes[gene] + random.uniform(-self.deviation,self.deviation)*(GENEMINMAXMEAN[gene][1]-GENEMINMAXMEAN[gene][0])),
                                    GENEMINMAXMEAN[gene][1],
                                    GENEMINMAXMEAN[gene][0]
-                               )
+                               ) #generate unique genes based of specGenes and deviation
         self.visibility = self.genes['attractiveness'] * math.sqrt(self.genes['neckLength'])/.7
     
     def get_target(self,r): #find all objects in radius
         found = []
         for obj in self.world.objects:
             #print(obj,doPythag(self.pos,obj.pos) <= r * (math.sqrt(obj.get_size())/5) * obj.visibility)
-            if doPythag(self.pos,obj.pos) <= r * (math.sqrt(obj.get_size())/7) * obj.visibility and self != obj:
+            if doPythag(self.pos,obj.pos) <= r * (math.sqrt(obj.get_size())/7) * obj.visibility and self != obj: #calc is arbitrary and may be altered
                 print(obj.genes,r * (math.sqrt(obj.get_size())/7) * obj.visibility)
                 found.append([
                     obj,
@@ -114,18 +115,20 @@ class Animal(Object): #basic animal
 
         return found
     
-    def get_size(self):
+    def get_size(self): #calculate size based on age
         return self.genes['size'] * cap(self.age * 4)
 
-    def tick(self):
+    def tick(self): #tick loop, add more here
         self.age += self.genes['ageSpeed']
 
-def gen_species(num,tp,world):
+def gen_species(num,tp,world): #generate <num> animals of type <tp> in <world>, all animals are one species.
     global GENEMINMAXMEAN
     for i in range(num):
         genes = { g:cap(random.normalvariate(GENEMINMAXMEAN[g][2],GENEMINMAXMEAN[g][3]), GENEMINMAXMEAN[g][1],GENEMINMAXMEAN[g][0]) for g in GENEMINMAXMEAN.keys() }
         Animal((random.uniform(0,world.size[0]),random.uniform(0,world.size[1])),world,specGenes=genes,deviation=random.uniform(0.002,0.1))
 
+
+#testing
 wld = World({},[1000,1000])
 gen_species(20,'herbivore',wld)
 
